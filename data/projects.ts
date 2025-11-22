3514fab08916cc7c9fdbc84b7fb6cc1833815c6d
Himanchal Kaushale <himanchal.kaushale@gmail.com>
2025-11-22 22:22:49 +0530

    refactor: remove link and logo fields from projects

---
+++ data/projects.ts
@@ -0,0 +1,36 @@
+import { Project } from '@/types';
+
+export const projects: Project[] = [
+  {
+    title: 'GitSnipper',
+    description:
+      'A modern, responsive web app to download any GitHub directory as a ZIP file, a single directly in your browser.',
+    image: '/projects/gitsnniper.vercel.app.png',
+    liveUrl: 'https://gitsnipper.vercel.app',
+    githubUrl: 'https://github.com/himanchalkaushale/Get-Github-Downloader',
+  },
+  {
+    title: 'Json-Prompt-Generator',
+    description:
+      'JSON Prompt Generator — build parameterized JSON prompts from reusable templates with a dynamic form and live preview.',
+    image: '/projects/jsonpromptgenerator.png',
+    liveUrl: 'https://jsonprompthk.vercel.app',
+    githubUrl: 'https://github.com/himanchalkaushale/Json_Prompt_Generator_Tool',
+  },
+  {
+    title: 'Refokus',
+    description:
+      'React-based clone website of Refokus. The project aims to replicate the layout, design, and functionalities of the original Refokus website',
+    liveUrl: 'https://refokus-himanchal.vercel.app/',
+    image: '/projects/refokusclone.png',
+    githubUrl: 'https://github.com/himanchalkaushale/Refokus-Clone',
+  },
+  {
+    title: 'ObysAgency',
+    description:
+      'Obys agency clone using HTML,CSS and Vanilla Js',
+    image: '/projects/obysagency.webp',
+    githubUrl: 'https://github.com/himanchalkaushale/ObysAgencyClone',
+    liveUrl: 'https://obysagencyhk.vercel.app',
+  },
+];
